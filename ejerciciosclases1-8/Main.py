db34769024f3603c3fe168fb63b3e3e654730e9e
javierpersan <61273246+javierpersan@users.noreply.github.com>
2023-02-16 13:27:33 +0100

    check

--- ejerciciosclases1-8/Main.py
+++ ejerciciosclases1-8/Main.py
@@ -1,24 +1,26 @@
 import MySQLdb
 class Conexion:
-    DB_HOST = 'localhost'
-    DB_USER = 'root'
-    DB_PASS = ''
-    DB_NAME = 'primitiva'
 
-    def __init__(self,query=''):
-        print("conexion abierta")
-        datos = [self.DB_HOST, self.DB_USER, self.DB_PASS, self.DB_NAME]
+    def __init__(self):
+        DB_HOST = 'localhost'
+        DB_USER = 'root'
+        DB_PASS = 'mysqlroot'
+        DB_NAME = 'a'
 
-        conn = MySQLdb.connect(*datos)
-        cursor = conn.cursor()
-        cursor.execute(query)
-        if query.upper().startswith('SELECT'):
-            data = cursor.fetchall()
-        else:
-            conn.commit()
-            data = None
+    def run_query(self ,query=''):
+            datos = [self.DB_HOST, self.DB_USER, self.DB_PASS, self.DB_NAME]
 
-        cursor.close()
-        conn.close()
+            conn = MySQLdb.connect(*datos)  # Conectar a la base de datos
+            cursor = conn.cursor()  # Crear un cursor
+            cursor.execute(query)  # Ejecutar una consulta
 
-        return data
+            if query.upper().startswith('SELECT'):
+                data = cursor.fetchall()  # Traer los resultados de un select
+            else:
+                conn.commit()  # Hacer efectiva la escritura de datos
+                data = None
+
+            cursor.close()  # Cerrar el cursor
+            conn.close()  # Cerrar la conexión
+
+            return data
